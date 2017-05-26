7c49a0621ebad71219253aed03133b8e8a0adeca
WillHL <will.horelacy@gmail.com>
2017-05-26 21:48:12 +1000

    Plot 1-3 created with png files

--- plot1.R
+++ plot1.R
@@ -1,12 +1,14 @@
 ## R script for creating plots from Electric power consumption data for Exploratory Data Analysis course.
 
 # Download data if required
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "power.zip",method= "curl")
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+              "power.zip",method= "curl")
 unzip("power.zip")
 
-# Read data
+# Read data if required
 colclass <- c(rep("character",2),rep("numeric",7))
-powerDF <- read.table("household_power_consumption.txt", header=TRUE, sep=";", colClasses=colclass, na.strings = "?")
+powerDF <- read.table("household_power_consumption.txt", header=TRUE, sep=";", 
+                      colClasses=colclass, na.strings = "?")
 
 # Format data
 powerDF$Date <- as.Date(powerDF$Date, format="%d/%m/%Y")
@@ -14,32 +16,13 @@ powerDF <- subset(powerDF, Date>="2007-02-01" & Date<="2007-02-02")
 powerDF$DateTime <- as.POSIXlt(paste(powerDF$Date, powerDF$Time), format="%Y-%m-%d %H:%M:%S")
 powerDF$Time <- strptime(powerDF$Time, format="%H:%M:%S")
 
+
+## Plot 1 ##
+
 # Set plot area
 par(mfrow=c(1,1))
 
 # Plot 1 - histogram Global Active Power to png
 png("plot1.png", width=480, height=480)
   hist(powerDF$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
-
-# Plot 2 - Global Active Power ~ Time to png
-png("plot2.png", width=480, height=480)
-  with(powerDF, 
-       plot(DateTime, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
-       )
-dev.off()
-
-
-# Set-up legend names
-legtxt <- names(powerDF)[grep("Sub_", names(powerDF))]
-
-# Plot 3 of sub_metering
-png("plot3.png", width=480, height=480)
-
-  with(powerDF, {
-    plot(DateTime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
-    lines(DateTime, Sub_metering_2, col="red")
-    lines(DateTime, Sub_metering_3, col="blue")
-    legend("topright", legend=legtxt, lty = 1, col=c("black","red","blue"))
-  })
 dev.off()
